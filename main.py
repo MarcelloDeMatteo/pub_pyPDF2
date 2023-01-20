2d32a9eb94b5b6f5500c684d117e77b322bf6530
MarcelloDeMatteo <marcello@dematteo.ch>
2023-01-20 20:25:53 +0100

    split the whole file original.pdf

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from PyPDF2 import PdfReader
+#!/usr/bin/env python3
+"""
+Splitting pdf into single page pdfs and renaming the output files
+"""
 
-reader = PdfReader("original.pdf")
-number_of_pages = len(reader.pages)
-page = reader.pages[0]
-text = page.extract_text()
+from PyPDF2 import PdfReader, PdfWriter
 
-print(text)
+# Load the pdf to the PdfFileReader object with default settings
+FILENAME = "original.pdf"
+
+# Split each page to separate pdf
+with open(FILENAME, "rb") as pdf_file:
+    pdf_reader = PdfReader(pdf_file)
+
+    for page_nr, page in enumerate(pdf_reader.pages, start=1):
+        pdf_writer = PdfWriter()
+        pdf_writer.add_page(page)
+
+        # Set the name of the extracted page
+        temp_name = f"{FILENAME[:-4]}-Page{page_nr}.pdf"
+        with open(temp_name, "wb") as split_page:
+            pdf_writer.write(split_page)
